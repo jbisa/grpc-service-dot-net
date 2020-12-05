89241d7136f3feb8ac07c44c2f3778a873c97420
Jay Bisa <jay.bisa@gmail.com>
2020-12-04 21:37:10 -0500

    Set up server and client apps for grpc PoC in .NET 5.0.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // A channel represents a connection from the client to the server
+            var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+
+            /*
+            var response = await client.SayHelloAsync(new HelloRequest
+            {
+                Name = "Niam Bisa"
+            });
+
+            Console.WriteLine($"From server: {response.Message}");*/
+
+            var call = client.SayHelloStream(new HelloRequest
+            {
+                Name = "Niam Bisa"
+            });
+
+            await foreach (var item in call.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine($"From server: {item.Message}");
+            }
+        }
+    }
+}
